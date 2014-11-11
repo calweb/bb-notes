bcc42324bafd2432af1226b0755b08cd90f1e59e
Calvin Webster <calweb@gmail.com>
2014-11-11 15:12:43 -0500

    some comments in code

--- day7/app.js
+++ day7/app.js
@@ -1,6 +1,11 @@
+// globals for demo
 var people;
 var appView;
 $(function () {
+  //we need to create collection from our raw data
+  // so we can do backbone things with it
   people = new ContactCollection(contacts);
+
+  // instantiate our collection view
   appView = new ContactsView({collection: people});
 });
