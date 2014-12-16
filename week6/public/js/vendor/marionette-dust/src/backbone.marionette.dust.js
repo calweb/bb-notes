f769c720c957b418fc5c206e262e0da4f83c7da1
Calvin Webster <calweb@gmail.com>
2014-12-16 15:23:18 -0500

    removed vendor files

--- week6/public/js/vendor/marionette-dust/src/backbone.marionette.dust.js
+++
@@ -1,9 +0,0 @@
-Backbone.Marionette.Renderer.render = function (template, data) {
-    var html;
-    // Template must be compiled and in the dust cache. Recommend pre-compiling
-    // and loading the templates as scripts at app start.
-    dust.render(template, data, function (err, out) {
-        html = out;
-    });
-    return html;
-};
